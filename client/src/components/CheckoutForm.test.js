c54f80208bb1ed9d9f2edce7f0db481f31643c91
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-05-08 10:38:47 -0700

    All tests are passing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,19 +1,43 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
-    //Arrange
+    //rendering the CheckoutForm Component
     const { getByText } = render(<CheckoutForm />)
-    //Act   
+    //querying for the form header "Checkout Form"
     const headerForm = getByText(/checkout form/i)
-    //Assert
+    //asserting that the form header is rendered
     expect(headerForm).toBeInTheDocument()
 
 });
 
-test("form shows success message on submit with form details", () => {
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+test("form shows success message on submit with form details", () => {
+    //rendering the CheckoutForm Component
+    const { getByTestId } = render(<CheckoutForm/>)
+    
+    //querying for the submit button 
+    const submitButton = getByTestId(/checkout/i)
+    //simulating a button click
+    fireEvent.click(submitButton)
+    //asserting the success message is being rendered
+    const successMessage = getByTestId(/successMessage/i)
+    expect(successMessage).toBeInTheDocument()
+
+}); 
